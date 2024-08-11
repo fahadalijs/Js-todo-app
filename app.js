04a3a412368aa5a51e3b9ef26c39312271e4d8e0
fahadalijs <fahad9827022@gmail.com>
2024-08-11 13:20:41 +0500

    Js todo app

---
+++ app.js
@@ -0,0 +1,62 @@
+// console.log(hello world);
+
+
+
+
+function renderTodo() {
+    ol.innerHTML = ''
+    for (let i = 0; i < arr.length; i++) {
+        ol.innerHTML += `
+        <li> ${arr[i]}
+        <button onclick="deleteTodo(${i})">delete</button>
+        <button onclick="editTodo(${i})">edit</button>
+        </li>
+        `
+    }
+}
+
+
+
+
+
+
+
+
+
+const todoInput = document.querySelector('#todo');
+const ol = document.querySelector('ol');
+
+const arr = [];
+
+function addTodo() {
+    arr.push(todoInput.value);
+    renderTodo()
+    todoInput.value = ''
+}
+
+
+
+function deleteTodo(index) {
+    arr.splice(index, 1);
+    renderTodo()
+}
+
+function editTodo(index) {
+    const updatedVal = prompt('enter updated value');
+    arr.splice(index , 1 , updatedVal);
+    renderTodo()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
